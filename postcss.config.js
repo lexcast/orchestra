2786d48d3053e11203d8a0feaaf917d6331b4cef
Daniel Alejandro Cast <lexcast13@gmail.com>
2020-05-22 02:31:55 -0500

    Update tailwindcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,1 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.js", "./public/*.html"],
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-});
-
-module.exports = {
-  plugins: [
-    require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
-  ]
-};
+module.exports = { plugins: [require("tailwindcss"), require("autoprefixer")] };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  purge: ["./src/**/*.js", "./public/*.html"],
   theme: {
     extend: {
       fontFamily: {

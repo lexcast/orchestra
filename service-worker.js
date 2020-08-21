ce9e478ba5f3e36166e4ba3dc00acc1124c3cbff
lexcast <lexcast@users.noreply.github.com>
2020-08-21 03:54:26 +0000

    deploy: 3d5e716a57785e7848bfa5bca24e9f2f8f15f351

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/orchestra/precache-manifest.9c88ae86a6475959633c3fb4959171b9.js"
+  "/orchestra/precache-manifest.f7a5657cd1697fa56d84059dec3def7f.js"
 );
 
 self.addEventListener('message', (event) => {
